4e538fb9a2abff56584474686b95fa9cee2dc6aa
briandang-wanco <brian.dang@wanco.com>
2026-03-05 14:53:35 -0700

    Nav Name Job Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Link, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function NameTitle({name}) {
+  return <h1>{name}</h1>
 }
 
-export default App
+function JobTitle({job}) {
+  return <h2>{job}</h2>
+}
+
+function HomePage({name, job}) {
+  return (
+    <div>
+      <NameTitle name={name} />
+      <JobTitle job={job} />
+    </div>
+  );
+}
+
+function AboutPage() {
+  return <h1>About Page</h1>
+}
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage name="Brian Dang" job="Software Engineer"/>} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>}/>
+      </Routes>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
